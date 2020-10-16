2d65acd87bb4341a5695cc262c36c81ac2b475e1
dozgr95 <dominik.zgraggen@hotmail.com>
2020-10-16 16:10:29 +0200

    update store with better types and cooking steps

--- 1topf/src/store/index.ts
+++ 1topf/src/store/index.ts
@@ -1,8 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { generateNewRecipe, Recipe } from './recipeGenerator';
+import {
+  Additional, generateNewRecipe, Grain, Legume, Recipe, Veggie,
+} from './recipeGenerator';
 
 Vue.use(Vuex);
+
+export interface RecipeState{
+  veggies: Veggie[];
+  legumes: Legume[];
+  grains: Grain[];
+  additionals: Additional[];
+  cookingSteps: string[];
+}
 
 export default new Vuex.Store({
   state: {
@@ -10,19 +20,23 @@ export default new Vuex.Store({
     legumes: [],
     grains: [],
     additionals: [],
-  },
+    cookingSteps: [],
+  } as RecipeState,
   mutations: {
-    setVeggies(state, veggies) {
+    setVeggies(state, veggies: Veggie[]) {
       state.veggies = veggies;
     },
-    setLegumes(state, legumes) {
+    setLegumes(state, legumes: Legume[]) {
       state.legumes = legumes;
     },
-    setGrains(state, grains) {
+    setGrains(state, grains: Grain[]) {
       state.grains = grains;
     },
-    setAdditionals(state, additionals) {
+    setAdditionals(state, additionals: Additional[]) {
       state.additionals = additionals;
+    },
+    setCookingSteps(state, steps: string[]) {
+      state.cookingSteps = steps;
     },
   },
   actions: {
@@ -38,6 +52,22 @@ export default new Vuex.Store({
       this.commit('setGrains', grains);
       this.commit('setAdditionals', additionals);
     },
+    generateAdvancedRecipe() {
+      const {
+        veggies,
+        legumes,
+        grains,
+        additionals,
+      }: Recipe = generateNewRecipe(2, 1, 1, 3);
+      this.commit('setVeggies', veggies);
+      this.commit('setLegumes', legumes);
+      this.commit('setGrains', grains);
+      this.commit('setAdditionals', additionals);
+    },
+    generateCookingSteps() {
+      // analyze props of fooditems (in recipe)
+      this.commit('setCookingSteps', ['hello 1', 'hello 2']);
+    },
   },
   getters: {
     recipe(state): Recipe {
@@ -47,6 +77,9 @@ export default new Vuex.Store({
         grains: state.grains,
         additionals: state.additionals,
       };
+    },
+    cookingSteps(state): string[] {
+      return state.cookingSteps;
     },
   },
   modules: {
