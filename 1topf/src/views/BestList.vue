266d036bc3552985fec610f9e8c4865703e3b2cc
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-23 10:03:29 +0100

    first version of best of foods

---
+++ 1topf/src/views/BestList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="best-list">
+      - Tomatensauce, Sugo, Pellati, Tube Tomaten-Püre
+      - Apfelessig (gesünder als Weinessig)
+      - Rapsöl für Bratpfanne (und weitere hochwertige Öle für andere Verwendung)
+      - Erbsen (Tiefgekühlt)
+      - Gemüse-Bouillon (und/oder Misopaste)
+      - Buchweizen, Hirse, Quinoa, Couscous, Bulgur
+      - Kircherbsen (Dose oder Roh)
+      - Zwiebeln, Knoblauch, Lauch
+      - Gewürze
+      - Nüsse
+      - Soja (und Lupinen)-Schrot/Granulat
+      - Dosen: Mais, Pilz, ?
+      -
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class BestList extends Vue {}
+</script>
